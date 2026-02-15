5bc2d4560b1b292d6e6d93dcf1f12847661b2597
Jstyler2012 <jstyler2012@gmail.com>
2026-02-14 20:51:22 -0500

    Update proscore.js

--- netlify/functions/proscore.js
+++ netlify/functions/proscore.js
@@ -1,22 +1,11 @@
-exports.handler = async function(event, context) {
-  try {
-    const response = await fetch("https://api.sportsdata.io/golf/v2/json/Tournaments", {
-      headers: {
-        "Ocp-Apim-Subscription-Key": process.env.SPORTSDATAIO_API_KEY
-      }
-    });
-
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    };
-
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ok: true,
+      message: "proscore function is alive",
+      query: event.queryStringParameters || {},
+    }),
+  };
 };
