ce79b466b22fc05f52b958044c7478094cd31d98
Jstyler2012 <jstyler2012@gmail.com>
2026-02-14 20:50:07 -0500

    Update admin.js

--- netlify/functions/admin.js
+++ netlify/functions/admin.js
@@ -1,1 +1,12 @@
-admin.js
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ok: true,
+      message: "admin function is alive",
+      path: event.path,
+      method: event.httpMethod,
+    }),
+  };
+};
