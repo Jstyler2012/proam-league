41a157943864ec1884e93b2f10ae7da74ff0b144
Jstyler2012 <jstyler2012@gmail.com>
2026-02-17 01:47:00 -0500

    Update proscore.js

--- netlify/functions/proscore.js
+++ netlify/functions/proscore.js
@@ -1,11 +1,16 @@
+// netlify/functions/proscore.js
 exports.handler = async (event) => {
+  // Placeholder:
+  // Later we can wire this to a real data source to pull PGA scores.
+  const q = event.queryStringParameters || {};
   return {
     statusCode: 200,
     headers: { "content-type": "application/json" },
     body: JSON.stringify({
       ok: true,
-      message: "proscore function is alive",
-      query: event.queryStringParameters || {},
+      pro_id: q.pro_id || null,
+      pro_to_par: null,
+      note: "Placeholder. Wire to PGA score provider later.",
     }),
   };
 };
